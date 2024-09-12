a821b46e9026c2cdcb0a2131ecc31f2b6d936352
borgeslais <borgescrlais@gmail.com>
2024-09-12 08:40:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Função de multiplicação
+function multiplica(a, b) {
+    return a * b;
+}
+// Função de saudação
+function ola(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplica(6, 8));
+console.log(ola("Ana"));
---
+++ index.ts
@@ -0,0 +1,12 @@
+// Função de multiplicação
+function multiplica(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function ola(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(multiplica(6, 8));
+console.log(ola("Ana"));
